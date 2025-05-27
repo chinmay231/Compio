bd25630f81678fc5139a998b2d61bc29b87c1d4e
chinmay231 <53120035+chinmay231@users.noreply.github.com>
2025-05-26 22:04:16 -0700

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-main
+from fastapi import FastAPI
+from app.routes.enlighten import router as enlighten_router
+from app.routes.generate import router as generate_router
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI(title="Compio API")
+app.include_router(enlighten_router, prefix="/api")
+app.include_router(generate_router, prefix="/api")
+
+frontend_dir = os.path.join(os.path.dirname(__file__), "../frontend")
+app.mount("/", StaticFiles(directory=frontend_dir, html=True), name="frontend")
